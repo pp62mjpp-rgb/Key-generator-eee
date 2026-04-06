4fcc7db5159a1c31e11ccf526023dac242f34b51
pp62mjpp-rgb <pp62mjpp@gmail.com>
2026-04-06 21:59:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1754,4 +1754,4 @@ def api_eat_to_access():
     return jsonify({'success': False, 'error': result}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
